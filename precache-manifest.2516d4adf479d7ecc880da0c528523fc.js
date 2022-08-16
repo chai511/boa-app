eac68b64ea9724e2d06a100d5817effa2a7f4687
Chaithanya Burra <chaithanyabsrk@gmail.com>
2022-08-16 09:51:57 +0530

    Updates

---
+++ precache-manifest.2516d4adf479d7ecc880da0c528523fc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f9d3634c117ab31e3145a9ec79c9864",
+    "url": "/boa-app/index.html"
+  },
+  {
+    "revision": "393814a21b2cb1f1ba6d",
+    "url": "/boa-app/static/css/main.226c9ba4.chunk.css"
+  },
+  {
+    "revision": "972daab8a71afb5c7d66",
+    "url": "/boa-app/static/js/2.ad7c57d2.chunk.js"
+  },
+  {
+    "revision": "abf6fcace3adbf8ab345683d164de87d",
+    "url": "/boa-app/static/js/2.ad7c57d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "393814a21b2cb1f1ba6d",
+    "url": "/boa-app/static/js/main.186f8ca3.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/boa-app/static/js/main.186f8ca3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9894c32db37d55a1e893",
+    "url": "/boa-app/static/js/runtime-main.485ec7c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/boa-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/boa-app-gh-pages/precache-manifest.02d6f3fa90d77a0dc03d325fd576915e.js"
+  "/boa-app/precache-manifest.2516d4adf479d7ecc880da0c528523fc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/boa-app-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/boa-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
